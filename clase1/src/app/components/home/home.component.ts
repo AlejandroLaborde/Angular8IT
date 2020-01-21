e0a86db1e5bc74cd31584378b9b8fb0654ad2364
aleer <alee_2695@live.com.ar>
2020-01-20 21:58:16 -0300

    primer clase

--- clase1/src/app/components/home/home.component.ts
+++ clase1/src/app/components/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private mostrar = true;
+  private dias: string[] = ['dia1', 'dia2'];
+  private diaSeleccionado = false;
+  private seleccionado: string ;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  showDia(dia: string): void {
+    this.seleccionado = dia;
   }
 
 }
